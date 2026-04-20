f5b634d332f5c82c8f41d82f6d9eef23c9cfdcac
Parsa <parsa.ahmed35@gmail.com>
2026-04-20 15:11:42 +0200

    Remove get_details method from ResearchGroup
    Removed the get_details method from ResearchGroup class.

--- src/lib/database/neo4j/ResearchGroup.py
+++ src/lib/database/neo4j/ResearchGroup.py
@@ -139,11 +139,3 @@ class Neo4JResearchGroup(ResearchGroupABC):
         "" 
         self.insert(research_group)
 
-
-    def get_details(self) -> List[ResearchGroupModel]:
-        query = (
-            "MATCH (rg:ResearchGroup) "
-            "RETURN properties(rg) ORDER BY rg.created_at DESC"
-        )
-        r = self._driver.execute_query(query, routing_="r", result_transformer_=Result.value)
-        return [ResearchGroupModel(**rg) for rg in r]
